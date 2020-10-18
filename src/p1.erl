b6e15f4e2a4815028ffddcb35ad47b50bde0f705
Diego <dcapro170_@estudiantec.cr>
2020-10-18 00:35:08 -0600

    [+] cambios menores

--- src/p1.erl
+++ src/p1.erl
@@ -1,10 +1,10 @@
 -module(p1).
 -export([geneticosNReinas/1, generaPoblacion/2]). 
 
-geneticosNReinas(1) -> [1];
+geneticosNReinas(N)when N<4 -> ["no existen respuestas para N = "| N];
 geneticosNReinas(N) -> main(N,4,400,generaPoblacion(generaLista(N),N*4)).
 
-main(N,Muestra,_Cruce,P) -> seleccion_respuesta(generacion(P, N, N*Muestra),P).
+main(N,Muestra,Cruce,P) -> seleccion_respuesta(generacion(P, N, N*Muestra),P,N,N*Muestra,Cruce).
 
 generaLista(N) when 0 >= N -> [];
 generaLista(N) -> [N|generaLista(N-1)].
@@ -15,16 +15,37 @@ generaPoblacion(L,CantPoblacion)->[shuffleList(L)|generaPoblacion(L,CantPoblacio
 shuffleList(L) -> [X||{_,X} <- lists:sort([ {rand:uniform(), N} || N <- L])].
 
 generacion(_Poblabcion, _N, 0)-> [];
-generacion([H|T], N, CantPoblacion)-> [fitnes1(H)|generacion(T,N,CantPoblacion-1)].
+generacion([H|T], N, CantPoblacion)-> [fitnes(H)|generacion(T,N,CantPoblacion-1)].
 
-fitnes1([H|T]) -> fitnes1(H,T,0,0,false,0).
-fitnes1(_Val,[], _I, _J, _Cond, Apt) -> Apt;
-fitnes1(Val,[H|T], I, J, _Cond, Apt)when I=:=J -> fitnes1(Val, T, I, J+1, condi_fitness(abs((I+1)-(J+1)) , abs(Val-H)), Apt);
-fitnes1(Val,[H|T], I, J, Cond, Apt) when Cond =:= true-> fitnes1(Val, T, I, J+1, condi_fitness(abs((I+1)-(J+1)) , abs(Val-H)), Apt+1);
-fitnes1(Val,[H|T], I, J, _Cond, Apt) -> fitnes1(Val, T, I, J+1, condi_fitness(abs((I+1)-(J+1)) , abs(Val-H)), Apt).
+fitnes([],_I,Col)-> Col;
+fitnes([H|T],I,Col) -> fitnes(T,H,T,I+1,0,true,Col).
+fitnes([H|T])-> fitnes(T,H,T,0,0,true,0).
+
+fitnes(TCromo,_Val,[], I, _J, _Cond, Col) -> fitnes(TCromo,I,Col);
+fitnes(TCromo,Val,[H|T], I, J, _Cond, Col)when I=:=J -> fitnes(TCromo,Val, T, I, J+1, condi_fitness(abs((I+1)-(J+1)) , abs(Val-H)), Col);
+fitnes(TCromo,Val,[H|T], I, J, Cond, Col) when Cond =:= true-> fitnes(TCromo,Val, T, I, J+1, condi_fitness(abs((I+1)-(J+1)) , abs(Val-H)), Col+1);
+fitnes(TCromo,Val,[H|T], I, J, _Cond, Col) -> fitnes(TCromo,Val, T, I, J+1, condi_fitness(abs((I+1)-(J+1)) , abs(Val-H)), Col).
+
+
 
 condi_fitness(A,B) when A =:= B-> true;
 condi_fitness(_A,_B) -> false.
 
-seleccion_respuesta(L,P)->[[L]|[P]].
 
+
+
+seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],N,CantPoblacion,Cruce) -> seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],[_Col|TCol],[_Cromo|Tcromo],N,CantPoblacion,Cruce).
+
+seleccion_respuesta([],[],Colisiones,Poblacion,N,CantPoblacion,Cruce)-> ciclo_cruce(Poblacion, Colisiones,N,CantPoblacion,Cruce);
+seleccion_respuesta([Col|_TCol],[Cromo|_Tcromo],_Colisiones,_Poblacion,_N,_CantPoblacion,_Cruce)when Col =:= 0 -> Cromo;
+seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],Colisiones,Poblacion,N,CantPoblacion,Cruce) -> seleccion_respuesta(TCol,Tcromo,Colisiones,Poblacion,N,CantPoblacion,Cruce).
+
+
+ciclo_cruce(Poblacion,_Colisiones,_N,_CantPoblacion,_Cruce) -> Poblacion.
+
+encuentraElite([H|_T],Val) when Val=:=H -> H;
+encuentraElite([_H|T],Val) -> encuentraElite(T,Val).
+
+encuentraPadre(_Poblacion,N)->N.
+
+cruce(Poblacion,_Elite,_Padre,_Madre) ->Poblacion.
