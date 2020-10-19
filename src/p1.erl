ceed2d20e98592417dbd74c65ca47e0ac61500fb
DIEGO <DCAPRO170_@HOTMAIL.COM>
2020-10-19 14:39:48 -0600

    [*] Comentarios y poco mas

--- src/p1.erl
+++ src/p1.erl
@@ -8,17 +8,23 @@ geneticosNReinas(N) -> main(N,4,700,generaPoblacion(generaLista(N),N*4),N,0.05).
 %funcion main para setear variables de funcionamiento del algoritmo (Muestras,Cruces,PorMuta)
 main(N,Muestra,Cruce,P,N,PorMuta) -> seleccion_respuesta(generacion(P, N, N*Muestra),P,N,N*Muestra,Cruce,PorMuta).
 
+%funcion que genera una lista ordenada del 1 al N ingresado 
 generaLista(N) when 0 >= N -> [];
 generaLista(N) -> [N|generaLista(N-1)].
 
+%funcion que genera una lista del uno al N desordenada
 generaPoblacion(_L,CantPoblacion) when 0 >= CantPoblacion -> [];
 generaPoblacion(L,CantPoblacion)->[shuffleList(L)|generaPoblacion(L,CantPoblacion-1)].
 
+%funcion que desordena aleatoriamente una lista
 shuffleList(L) -> [X||{_,X} <- lists:sort([ {rand:uniform(), N} || N <- L])].
 
+%Genera lista de valores de fitnes, entre menor sea el valor de colision mejor sera el individuo
 generacion(_Poblabcion, _N, 0)-> [];
 generacion([H|T], N, CantPoblacion)-> [fitnes(H)|generacion(T,N,CantPoblacion-1)].
 
+
+%funcion fitnes califica a un cromosoma por numero de coliciones diagonales y verticales (- coliones + apto)
 fitnes([],_I,Col)-> Col;
 fitnes([H|T],I,Col) -> fitnes(T,H,T,I+1,0,true,Col).
 fitnes([H|T])-> fitnes(T,H,T,0,0,true,0).
@@ -29,16 +35,16 @@ fitnes(TCromo,Val,[H|T], I, J, Cond, Col) when Cond =:= true-> fitnes(TCromo,Val
 fitnes(TCromo,Val,[H|T], I, J, _Cond, Col) -> fitnes(TCromo,Val, T, I, J+1, condi_fitness(abs((I+1)-(J+1)) , abs(Val-H)), Col).
 
 
-
+%funcion que retorna T si los ds valores entrantes son iguales si no F
 condi_fitness(A,B) when A =:= B-> true;
 condi_fitness(_A,_B) -> false.
 
 
-
-seleccion_respuesta(_Colisiones,Poblacion,_N,_CantPoblacion,0,_PorMuta)-> Poblacion;
+%recorre la poblacion si el valor de colision es 0 retorna ek resultado como solucion, si no ejecuta otra generacion hasta teminar con el numero de cruces si no se encuentra se retorna el valor elite de la ultima poblacion  
+seleccion_respuesta(_Colisiones,Poblacion,_N,_CantPoblacion,0,_PorMuta)-> ["NO"|Poblacion];
 seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],N,CantPoblacion,Cruce,PorMuta) -> seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],[_Col|TCol],[_Cromo|Tcromo],N,CantPoblacion,Cruce,PorMuta).
 
-seleccion_respuesta([],[],Colisiones,Poblacion,N,CantPoblacion,Cruce,PorMuta)-> ciclo_cruce(cicloMutacion(cruce(Poblacion,busquedaByValue(Poblacion,Colisiones,lists:min(Colisiones)),cortaPadre(encuentraPadre(Poblacion,rand:uniform(N)),N div 2,[]),encuentraPadre(Poblacion,rand:uniform(N)),[],CantPoblacion,N),round(CantPoblacion*PorMuta),CantPoblacion,N),N,CantPoblacion,Cruce-1,PorMuta);
+seleccion_respuesta([],[],Colisiones,Poblacion,N,CantPoblacion,Cruce,PorMuta)-> ciclo_cruce(cicloMutacion(cruce(Poblacion,busquedaByValue(Poblacion,Colisiones,lists:min(Colisiones)),cortaPadre(encuentraPadre(Poblacion,rand:uniform(N)),N div 2,[]),encuentraPadre(Poblacion,rand:uniform(N)),[],CantPoblacion,N),round(CantPoblacion*PorMuta-1),CantPoblacion,N),N,CantPoblacion,Cruce-1,PorMuta);
 seleccion_respuesta([Col|_TCol],[Cromo|_Tcromo],_Colisiones,_Poblacion,_N,_CantPoblacion,_Cruce,_PorMuta)when Col =:= 0 -> ["SOLUCION"|Cromo];
 seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],Colisiones,Poblacion,N,CantPoblacion,Cruce,PorMuta) -> seleccion_respuesta(TCol,Tcromo,Colisiones,Poblacion,N,CantPoblacion,Cruce,PorMuta).
 
@@ -63,7 +69,7 @@ cicloMutacion(Poblacion,CantMutaciones,CantPoblacion,N) -> cicloMutacion(mutacio
 
 
 mutacion([_PH|PT], Indice, N) -> mutacion(PT,Indice,[],N). 
-mutacion([PH|PT], 0,NuevaPoblacion,N) -> NuevaPoblacion++[mutaCromosoma(PH,rand:uniform(N),rand:uniform(N))]++PT;
+mutacion([PH|PT], 0,NuevaPoblacion,N) -> NuevaPoblacion++[mutaCromosoma(PH,rand:uniform(N),rand:uniform(N))]++[PH|PT];
 mutacion([PH|PT], Indice,NuevaPoblacion,N) -> mutacion(PT,Indice-1,[PH|NuevaPoblacion],N).
 
 
