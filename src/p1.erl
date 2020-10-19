f7632edf526a18e6fc6de9f09cc3f9e6f52e11cd
DIEGO <DCAPRO170_@HOTMAIL.COM>
2020-10-19 11:52:32 -0600

    [*] comentarios | sin funcionar

--- src/p1.erl
+++ src/p1.erl
@@ -1,10 +1,12 @@
 -module(p1).
--export([geneticosNReinas/1, generaPoblacion/2, cruce/7, generaLista/1, cortaPadre/3, encuentraPadre/2, encuentraElite/2, busquedaByValue/3, generacion/3, cicloMutacion/4, mutacion/3] ). 
+-export([geneticosNReinas/1, generaPoblacion/2, cruce/7, generaLista/1, cortaPadre/3, encuentraPadre/2, encuentraElite/2, busquedaByValue/3, generacion/3, cicloMutacion/4, mutacion/3, ciclo_cruce/5, mutaCromosoma/3,mutaCromosoma2/4] ). 
 
-geneticosNReinas(N)when N<4 -> ["no existen respuestas para N = "| N];
-geneticosNReinas(N) -> main(N,4,700,generaPoblacion(generaLista(N),N*4)).
+%funcion principal dispara el proceso
+geneticosNReinas(N)when N<4 -> ["no existen respuestas para N = "| N]; %si el N entrada es menor que 4 se retorna no hay respuesta
+geneticosNReinas(N) -> main(N,4,700,generaPoblacion(generaLista(N),N*4),N,0.05).
 
-main(N,Muestra,Cruce,P) -> seleccion_respuesta(generacion(P, N, N*Muestra),P,N,N*Muestra,Cruce).
+%funcion main para setear variables de funcionamiento del algoritmo (Muestras,Cruces,PorMuta)
+main(N,Muestra,Cruce,P,N,PorMuta) -> seleccion_respuesta(generacion(P, N, N*Muestra),P,N,N*Muestra,Cruce,PorMuta).
 
 generaLista(N) when 0 >= N -> [];
 generaLista(N) -> [N|generaLista(N-1)].
@@ -33,40 +35,43 @@ condi_fitness(_A,_B) -> false.
 
 
 
-seleccion_respuesta(Colisiones,Poblacion,_N,_CantPoblacion,0)-> Poblacion;
-seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],N,CantPoblacion,Cruce) -> seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],[_Col|TCol],[_Cromo|Tcromo],N,CantPoblacion,Cruce).
+seleccion_respuesta(_Colisiones,Poblacion,_N,_CantPoblacion,0,_PorMuta)-> Poblacion;
+seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],N,CantPoblacion,Cruce,PorMuta) -> seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],[_Col|TCol],[_Cromo|Tcromo],N,CantPoblacion,Cruce,PorMuta).
 
-seleccion_respuesta([],[],Colisiones,Poblacion,N,CantPoblacion,Cruce)-> ciclo_cruce(cicloMutacion(cruce(Poblacion,busquedaByValue(Poblacion,Colisiones,lists:min(Colisiones)),cortaPadre(encuentraPadre(Poblacion,rand:uniform(N)),N div 2,[]),encuentraPadre(Poblacion,rand:uniform(N)),[],CantPoblacion,N),round(CantPoblacion*0.05),CantPoblacion,N),N,CantPoblacion,Cruce-1);
-seleccion_respuesta([Col|_TCol],[Cromo|_Tcromo],_Colisiones,_Poblacion,_N,_CantPoblacion,_Cruce)when Col =:= 0 -> ["SOLUCION"|Cromo];
-seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],Colisiones,Poblacion,N,CantPoblacion,Cruce) -> seleccion_respuesta(TCol,Tcromo,Colisiones,Poblacion,N,CantPoblacion,Cruce).
+seleccion_respuesta([],[],Colisiones,Poblacion,N,CantPoblacion,Cruce,PorMuta)-> ciclo_cruce(cicloMutacion(cruce(Poblacion,busquedaByValue(Poblacion,Colisiones,lists:min(Colisiones)),cortaPadre(encuentraPadre(Poblacion,rand:uniform(N)),N div 2,[]),encuentraPadre(Poblacion,rand:uniform(N)),[],CantPoblacion,N),round(CantPoblacion*PorMuta),CantPoblacion,N),N,CantPoblacion,Cruce-1,PorMuta);
+seleccion_respuesta([Col|_TCol],[Cromo|_Tcromo],_Colisiones,_Poblacion,_N,_CantPoblacion,_Cruce,_PorMuta)when Col =:= 0 -> ["SOLUCION"|Cromo];
+seleccion_respuesta([_Col|TCol],[_Cromo|Tcromo],Colisiones,Poblacion,N,CantPoblacion,Cruce,PorMuta) -> seleccion_respuesta(TCol,Tcromo,Colisiones,Poblacion,N,CantPoblacion,Cruce,PorMuta).
 
 busquedaByValue([PH|_PT],[CH|_CT],Val) when CH =:= Val->PH;
 busquedaByValue([_PH|PT],[_CH|CT],Val)-> busquedaByValue(PT,CT,Val).
 
 
-ciclo_cruce(Poblacion,N,CantPoblacion,Cruce) -> seleccion_respuesta(generacion(Poblacion,N,CantPoblacion),Poblacion,N,CantPoblacion,Cruce).
+ciclo_cruce(Poblacion,N,CantPoblacion,Cruce,PorMuta) -> seleccion_respuesta(generacion(Poblacion,N,CantPoblacion),Poblacion,N,CantPoblacion,Cruce,PorMuta).
 
 encuentraElite([H|_T],Val) when Val=:=H -> H;
 encuentraElite([_H|T],Val) -> encuentraElite(T,Val).
 
 encuentraPadre([PH|_PT],0) -> PH;
-encuentraPadre([PH|PT],N)-> encuentraPadre(PT,N-1).
+encuentraPadre([_PH|PT],N)-> encuentraPadre(PT,N-1).
 
-cruce(_Poblacion,_Elite,_Padre,_Madre,NuevaPoblacion,0,N) -> NuevaPoblacion ;
+cruce(_Poblacion,_Elite,_Padre,_Madre,NuevaPoblacion,0,_N) -> NuevaPoblacion;%,round(CantPoblacion*0.05),length(CantPoblacion),N) ;
 cruce(Poblacion,Elite,Padre,Madre,[],CantPoblacion,N) -> cruce(Poblacion,Elite,Padre,Madre,[Elite],CantPoblacion-1,N);
 cruce(Poblacion,Elite,Padre,Madre,NuevaPoblacion,CantPoblacion,N) -> cruce(Poblacion,Elite,cortaPadre(encuentraPadre(Poblacion,rand:uniform(N))	,N div 2,[]),encuentraPadre(Poblacion,rand:uniform(N)),[Padre++Madre--Padre|NuevaPoblacion],CantPoblacion-1,N).
 
-cicloMutacion(Poblacion,0,CantPoblacion,N) -> Poblacion;
+cicloMutacion(Poblacion,0,_CantPoblacion,_N) -> Poblacion;
 cicloMutacion(Poblacion,CantMutaciones,CantPoblacion,N) -> cicloMutacion(mutacion(Poblacion,rand:uniform(CantPoblacion),N),CantMutaciones-1,CantPoblacion,N).
 
 
-mutacion([PH|PT], Indice, N) -> mutacion(PT,Indice,[],N). 
+mutacion([_PH|PT], Indice, N) -> mutacion(PT,Indice,[],N). 
 mutacion([PH|PT], 0,NuevaPoblacion,N) -> NuevaPoblacion++[mutaCromosoma(PH,rand:uniform(N),rand:uniform(N))]++PT;
 mutacion([PH|PT], Indice,NuevaPoblacion,N) -> mutacion(PT,Indice-1,[PH|NuevaPoblacion],N).
 
+
+mutaCromosoma(Cromosoma, 1, J) -> mutaCromosoma2(Cromosoma,[lists:nth(J,Cromosoma)]++lists:nthtail(1,Cromosoma)--lists:nthtail(J-1,Cromosoma),1,J);
+mutaCromosoma(Cromosoma, I, 1) -> mutaCromosoma2(Cromosoma,[lists:nth(I,Cromosoma)]++lists:nthtail(1,Cromosoma)--lists:nthtail(I-1,Cromosoma),1,I);
 mutaCromosoma(Cromosoma, I, J) when I<J -> mutaCromosoma2(Cromosoma,lists:sublist(Cromosoma,I-1)++[lists:nth(J,Cromosoma)]++lists:nthtail(I,Cromosoma)--lists:nthtail(J-1,Cromosoma),I,J);
 mutaCromosoma(Cromosoma, I, J) when J<I -> mutaCromosoma2(Cromosoma,lists:sublist(Cromosoma,J-1)++[lists:nth(I,Cromosoma)]++lists:nthtail(J,Cromosoma)--lists:nthtail(I-1,Cromosoma),J,I);
-mutaCromosoma(Cromosoma, I, J) when J=:=I -> mutaCromosoma(Cromosoma,I,J+1).
+mutaCromosoma(Cromosoma, I, I) -> mutaCromosoma(Cromosoma,I,I+1).
 mutaCromosoma2(Cromosoma, NCromosoma, I, J) -> NCromosoma++[lists:nth(I,Cromosoma)]++lists:nthtail(J,Cromosoma).
 
 cortaPadre(_Padre,0,DivPa) -> DivPa;
